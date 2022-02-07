26be1f8cedd54545ca0489bc8364d34c9d5ae56f
Aranduka <wilhelmiii2018@gmail.com>
2022-02-07 17:00:13 -0300

    Agregando dinamismo a insertar alumno

--- insertarAlumno.js
+++ insertarAlumno.js
@@ -7,24 +7,90 @@ if (!localStorage.getItem('token')) {
 
 // URL
 const url_alumnos = "http://localhost:8000/alumnos";
+const url_sacramentos = "http://localhost:8000/sacramentos";
 
 // Inicializar el HTML 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    listar_sacramentos();
     var elems = document.querySelectorAll('.datepicker');
-    var instances1 = M.Datepicker.init(elems);
-    var elems = document.querySelectorAll('.chips');
-    var instances = M.Chips.init(elems);
-    console.log(M.Chips.getInstance())
-  });
+    var instances = M.Datepicker.init(elems);
+});
 
+
+// Sacramentos
+
+const cbo_sacramentos = document.getElementById("sacramentos");
+
+const listar_sacramentos = async () => {
+
+    const solicitud = new Request(
+        url_sacramentos,
+        {
+            method: 'Get',
+            withCredentials: true,
+            credentials: 'include',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem("token"),
+                'Content-Type': 'application/json'
+            }
+        }
+    );
+    const respuesta = await fetch(solicitud);
+    const datos = await respuesta.json();
+    if (!respuesta.ok) {
+        alert("Algo fallo al listar los sacramentos");
+    }
+    else {
+
+        for (let dato of datos) {
+            let nueva_opcion = document.createElement("option");
+            nueva_opcion.value = dato.id_sacramento;
+            nueva_opcion.text = dato.descripcion;
+            cbo_sacramentos.appendChild(nueva_opcion);
+        }
+    }
+};
+
+const sacramentos_seleccionados = [];
+const chips_sacramentos_container = document.getElementById("sacramentos_seleccionados");
 
 
 const btn_agregar_sacramento = document.getElementById("agregar_sacramento");
-const chips_sacramentos = document.querySelectorAll(".chips");
-materialize_chips = M.Chips.getInstance(chips_sacramentos);
+btn_agregar_sacramento.addEventListener("click", function (e) {
+    if (sacramentos_seleccionados.includes(cbo_sacramentos.value)) {
+        alert("Sacramento ya agregado");
+    }
+    else {
 
-btn_agregar_sacramento.addEventListener("click", function(e){
-    materialize_chips.addChip({
-        tag: "Holii"
-    });
+        const html = `
+        <div class="chip">
+        ${cbo_sacramentos.options[cbo_sacramentos.selectedIndex].text}<span id="close_sacramento_${cbo_sacramentos.value}"><i class="material-icons close">close</i></span>
+        </div>
+        `;
+        sacramentos_seleccionados.push(cbo_sacramentos.value);
+        chips_sacramentos_container.innerHTML += html;
+
+        // Agregare un event listener dentro de un for para 
+        // iterar en el array con los valores y darles a cada uno que se pueda eliminar
+        // con un settimeout
+
+        for (sacramento of sacramentos_seleccionados) {
+            const eliminar_sacramento = document.getElementById(`close_sacramento_${sacramento}`);
+            eliminar_sacramento.onclick = () => {
+                let id = eliminar_sacramento.getAttribute("id");
+                let dato = id.charAt(id.length-1);
+                let indice = sacramentos_seleccionados.indexOf(dato);
+                sacramentos_seleccionados.slice(indice, indice + 1);
+                console.log(sacramentos_seleccionados);
+                console.log(dato);
+                console.log(indice);
+            }
+
+        }
+
+
+    }
 });
+
+
+
